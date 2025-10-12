6dcf33172f99d53a0c6f2ec58b5095cef129c207
Flovoko <79543418+Flovoko@users.noreply.github.com>
2025-10-12 16:25:07 +0200

    Update background.js

--- background.js
+++ background.js
@@ -18,13 +18,6 @@ const colorC = randomColor();
 const canvas = document.createElement('canvas');
 canvas.id = 'strw-bg';
 document.body.appendChild(canvas);
-canvas.style.position = 'fixed';
-canvas.style.top = '0';
-canvas.style.left = '0';
-canvas.style.width = '100%';
-canvas.style.height = '100%';
-canvas.style.zIndex = '-1';
-canvas.style.pointerEvents = 'none';
 
 const ctx = canvas.getContext('2d');
 let w, h, particles;
